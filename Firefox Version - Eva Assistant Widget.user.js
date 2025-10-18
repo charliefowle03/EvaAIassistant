2467d9781ce4dcab6aed9dcf50f9646b31c3e0ae
charliefowle03 <44536502+charliefowle03@users.noreply.github.com>
2025-10-18 07:30:12 -0400

    Bump version from 1.0 to 1.1

--- Firefox Version - Eva Assistant Widget.user.js
+++ Firefox Version - Eva Assistant Widget.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Eva Assistant Widget
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Eva Widget
 // @author       You
 // @match        *://*/*
