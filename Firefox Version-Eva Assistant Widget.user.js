2ee0f94c9338fff97865f35e60b1f6bd5dd1e9ca
charliefowle03 <44536502+charliefowle03@users.noreply.github.com>
2025-10-18 07:25:48 -0400

    Update URLs in user script for widget

--- Firefox Version-Eva Assistant Widget.user.js
+++ Firefox Version-Eva Assistant Widget.user.js
@@ -5,9 +5,8 @@
 // @description  Eva Widget
 // @author       You
 // @match        *://*/*
-// @updateURL    https://github.com/charliefowle03/EvaAIassistant/raw/refs/heads/main/Eva%20Assistant%20Widget.user.js
-// @downloadURL  https://github.com/charliefowle03/EvaAIassistant/raw/refs/heads/main/Eva%20Assistant%20Widget.user.js
-// @run-at       document-end
+// @updateURL    https://github.com/charliefowle03/EvaAIassistant/raw/refs/heads/main/Firefox%20Version-Eva%20Assistant%20Widget.user.js
+// @downloadURL  https://github.com/charliefowle03/EvaAIassistant/raw/refs/heads/main/Firefox%20Version-Eva%20Assistant%20Widget.user.js
 // ==/UserScript==
 
 (function() {
